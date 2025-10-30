92f261dcf9d24a8c007d6f64d79f0c6f75b94fd1
samuelArenas2005 <e.sir.samuel.arenas@cali.edu.co>
2025-10-29 23:07:06 -0500

    merge with miguel

---
+++ frontend/src/pages/SearchEvent/searchPage.js
@@ -0,0 +1,56 @@
+import {getAllEvents} from '../../API/api'
+
+
+
+function diaMes(iso) {
+  const d = new Date(iso);
+  return d.toLocaleDateString('es-ES', { day: 'numeric', month: 'long' });
+}
+
+function hora12Colombia(iso) {
+  const d = new Date(iso);
+  return d.toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+    timeZone: 'America/Bogota'
+  }).toLowerCase();
+}
+
+export const getEvents = async () => {
+  try {
+    // Llamada al backend
+    const response = await getAllEvents();
+    console.log("Respuesta cruda del backend:", response);
+    
+    // Asegurarnos de que tenemos un array para mapear
+    const events = Array.isArray(response) ? response : [];
+    
+    const formattedFromApi = events.map(event => ({
+      id: event.id || 0,
+      imageUrl: event.main_image || "https://via.placeholder.com/300x200",
+      category: event.categories?.[0]?.category || "Sin categoría",
+      title: event.title || "Sin título",
+      description: event.description || "Sin descripción",
+      date: event.start_date ? diaMes(event.start_date) : "Por definir",
+      time: event.start_date ? hora12Colombia(event.start_date) : "Por definir",
+      location: event.location_info || "Por definir",
+      currentParticipants: event.attendees_count || 0,
+      totalParticipants: event.capacity || 100,
+      organizer: event.creator?.username || "Desconocido",
+      onRegisterClick: () => alert(`Ver detalle de registro ${event.id}`),
+      onHeartClick: () => console.log(`Heart ${event.start_date}`),
+      showRegisterButton: true,
+      showHeartButton: true,
+      activeHeart: false,
+    }));
+
+    console.log("Eventos formateados:", formattedFromApi);
+    return formattedFromApi
+
+    console.log("Eventos crudos del backend:", events.data);
+  } catch (error) {
+    console.error("Error al obtener eventos:", error);
+    return []; // retornar array vacío en caso de error
+  }
+};
