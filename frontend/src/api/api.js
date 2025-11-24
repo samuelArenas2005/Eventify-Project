7b40fdb653fbe2d04b22e65cd6333eadf97a9e5d
samuelArenas2005 <e.sir.samuel.arenas@cali.edu.co>
2025-11-23 21:56:03 -0500

    Cambia el api.js para que importe la url del backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from './axiosConfig';
 
-const BASE_URL = 'http://127.0.0.1:8000/api/'
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/';
 const LOGIN_URL = `${BASE_URL}token/`
 const REFRESH_URL = `${BASE_URL}token/refresh/`
 const LOGOUT_URL = `${BASE_URL}token/logout/`
